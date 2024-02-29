c7a408711bc67c7667c23179f12dc3ccfa135251
Vittutexx <vitortex06@gmail.com>
2024-02-28 23:54:04 -0300

    Primeiros passos da aplicação android com react native

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Vitor Teixeira Silva RM: 552228 - 2TDSS</Text>
+      <Text style={styles.trabalhadores}>Trabalhadores do mundo inteiro, uni-vos! ☭</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    borderWidth: 3,
+    borderColor: '#F5DD61',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    backgroundColor: 'black',
+    padding: 10,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 10,
+  },
+  trabalhadores: {
+    marginTop: 10,
+    color: '#F5DD61',
+    fontWeight: 'bold',
   },
 });
