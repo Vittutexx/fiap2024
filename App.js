b0402150c84734dd67c471f3fd84f71574d205f5
Vittutexx <vitortex06@gmail.com>
2024-03-06 22:54:42 -0300

    Adicionando o InputText

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import {StatusBar, Text, View} from 'react-native';
+import {StatusBar, Text, TextInput, View} from 'react-native';
 
 
 
 export default class App extends React.Component {
   render() {
   return (
-      <View>
-        <View style={{backgroundColor: 'crimson', height: 200, width: 200}}></View>
-        <View style={{alignSelf: 'center', backgroundColor: 'cornflowerblue', borderRadius: 100, height: 200, marginVertical: 20, width: 200}}></View>
-        <View style={{alignSelf: 'center', backgroundColor: 'greenyellow', alignSelf: 'flex-end', height: 200, justifyContent: 'center', width: 300}}>
-          <Text>Texto de uma View</Text>
-        </View>
-        <StatusBar style="light" />
+      <View style={{ padding: 16 }}>
+        <TextInput
+          placeholder="Informe seu nome"
+          style={{
+            borderColor: 'gray',
+            borderWidth: 1,
+            height: 40,
+            paddingHorizontal: 16,
+          }} />
+        <StatusBar style = "light" />
       </View>
     );
   }
