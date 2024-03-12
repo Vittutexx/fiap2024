acb38529cb99d3baf5ac79bdfdcee6b9c360cb91
vittutexx <vitortex06@gmail.com>
2024-03-11 22:41:24 -0300

    colocando useState e setando a tela de login.

--- App.js
+++ App.js
@@ -1,22 +1,54 @@
 import  { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 
 
 export default function App(){
 
+  const [login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
+
+  function validarLogin(login, senha){
+    alert.alert(login);
+
+    const loginPadrao = "FIAP";
+    const senhaPadrao = "palmeirassemmundial";
+
+    if (login == loginPadrao && senha == senhaPadrao){
+      console.warn("Login efetuado com sucesso!");
+    } else {
+      console.warn("Não foi possivel efetuar o login.");
+    }
+  }
+
+
+
+
   return (
       <View style={styles.container}>
         <Text>Login</Text>
-        <TextInput
+        <TextInput 
+        style={styles.input}
         stakeholder = "Informe seu login"
-        value = "FIAP"
-        placeholderTextColor = '#999' />
+        value = {login}
+        placeholderTextColor = '#999'
+        onChangeText = { (e) => setLogin(e)} />
+        
+
         <Text>Senha</Text>
         <TextInput 
+        style={styles.input}
         placeholder='Informe sua senha'
-        value="palmeirasnaotemmundial"
-        placeholderTextColor={'#999'} />
+        value={senha}
+        placeholderTextColor={'#999'}
+        onChangeText = { (e) => setSenha(e)} />
+        
+
+        <Button
+          title = "Efetuar Login"
+          onPress = {(e) =>validarLogin(login, senha)} />
+
         <StatusBar style="auto" />
       </View>
     );
@@ -29,4 +61,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderWidth: 1,
+    width: '90%',
+    paddingLeft: 15,
+    marginBottom: 10
+  }
 });
